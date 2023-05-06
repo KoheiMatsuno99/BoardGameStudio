a7320cb6786dcbe7c2aadbff3ec32123f4ef2895
kohei <koheimtri3@gmail.com>
2023-05-06 23:15:32 +0900

    create board

---
+++ env/boardGameProject/frontend/src/Board.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Board.module.css";
+
+const Board: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.capturedPiecesTop}></div>
+            <div className={styles.board}>
+                {Array.from({length: 8}).map((_, row_i) => (
+                    <div key={'row' + row_i} className={styles.row}>
+                        {Array.from({length: 8}).map((_, col_i) => (
+                            <div key={'col' + col_i} className={styles.block}></div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className={styles.capturedPiecesBottom}></div>
+        </div>
+    )
+}
+
+export default Board;
