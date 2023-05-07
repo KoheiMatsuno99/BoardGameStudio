4949fd322f0a185fcb3360e5df9b5aa18adc1583
kohei <koheimtri3@gmail.com>
2023-05-07 23:02:15 +0900

    installed flake8 and black

--- env/boardGameProject/backend/geister/geister.py
+++ env/boardGameProject/backend/geister/geister.py
@@ -1,20 +1,28 @@
 import random
+
+
 class Piece:
     def __init__(self, OWNER: str, TYPE: str) -> None:
         self.__owner = OWNER
         self.__type = TYPE
         self.__position = None
-        assert TYPE in {"red", "blue"} , "Type must be either 'red' or 'blue'."
+        assert TYPE in {"red", "blue"}, "Type must be either 'red' or 'blue'."
+
     # ownerはPlayer.nameと紐づくことを想定しているが、オンラインモードで名前が衝突した時、バグの可能性あり
+
     def get_owner(self) -> str:
         return self.__owner
+
     def set_owner(self, owner: str) -> None:
         self.__owner = owner
+
     def get_type(self) -> str:
         return self.__type
+
     # Pieceのtypeは不変なのでsetterは定義しない
     def get_position(self) -> list[int] or None:
         return self.__position
+
     def set_position(self, position: list[int] or None) -> None:
         self.__position = position
 
@@ -23,38 +31,53 @@ class Block:
     def __init__(self, ADDRESS: list[int]) -> None:
         self.__address = ADDRESS
         self.__piece = None
-    
+
     def get_address(self) -> list[int]:
         return self.__address
+
     # Blockに付与するaddressは不変なのでsetterは定義しない
     def set_piece(self, piece: Piece or None) -> None:
         self.__piece = piece
+
     def get_piece(self) -> Piece or None:
         return self.__piece
+
     # 四隅のマスは脱出可能なマスとする
     def is_escape_block(self) -> bool:
         # todo より良い書き方があれば修正
         # 8✖️8のボードを想定
-        return self.__address == [0, 0] or self.__address == [0, 7] or self.__address == [7, 0] or self.__address == [7, 7]
+        return (
+            self.__address == [0, 0]
+            or self.__address == [0, 7]
+            or self.__address == [7, 0]
+            or self.__address == [7, 7]
+        )
+
 
 class Player:
     __picked_red_pieces_count: int = 0
     __picked_blue_pieces_count: int = 0
+
     # todo オンライン対戦時に名前被りで衝突する可能性があるので、名前ではなくidに変更or追加
-    # idはプレイヤーの戦績を管理するPlayerInfoクラスからとってくる 
+    # idはプレイヤーの戦績を管理するPlayerInfoクラスからとってくる
     def __init__(self, name: str) -> None:
         self.name = name
         self.pieces: dict[str, Piece] = {
-            f'{self.name}_blue_{i}': Piece(self.name, "blue") for i in range(4) 
+            f"{self.name}_blue_{i}": Piece(self.name, "blue") for i in range(4)
         }
-        self.pieces.update({f'{self.name}_red_{i}': Piece(self.name, "red") for i in range(4)})
+        self.pieces.update(
+            {f"{self.name}_red_{i}": Piece(self.name, "red") for i in range(4)}
+        )
 
     def get_name(self) -> str:
         return self.name
+
     def get_picked_red_pieces_count(self) -> int:
         return self.__picked_red_pieces_count
+
     def get_picked_blue_pieces_count(self) -> int:
         return self.__picked_blue_pieces_count
+
     def add_picked_pieces_count(self, piece: Piece) -> None:
         piece_type: str = piece.get_type()
         # 引数がPieceクラスのインスタンスなので、
@@ -65,19 +88,25 @@ class Player:
         else:
             self.__picked_red_pieces_count += 1
             print("赤いオバケを取ってしまった...。")
-    
+
+
 class Table:
     def __init__(self, players: list[Player]) -> None:
         self.__players = players
-        self.__table: list[list[Block]] = [[Block([x, y]) for y in range(8)] for x in range(8)]
+        self.__table: list[list[Block]] = [
+            [Block([x, y]) for y in range(8)] for x in range(8)
+        ]
         self.__winner: str = ""
 
     def get_players(self) -> list[Player]:
         return self.__players
+
     def get_table(self) -> list[list[Block]]:
         return self.__table
+
     def get_winner(self) -> str:
-        return self.__winner 
+        return self.__winner
+
     # 各プレイヤーがコマの初期位置を決定するメソッド
     def initialize_pieces_position(self) -> None:
         for i in range(self.__players):
@@ -97,6 +126,7 @@ class Table:
                         self.__table[x][y].set_piece(piece)
                         piece.set_position([x, y])
                         break
+
     def get_piece_at(self, position: list[int]) -> Piece or None:
         return self.__table[position[0]][position[1]].get_piece()
 
@@ -104,8 +134,10 @@ class Table:
     # destinationに相手の駒がある時に呼び出す
     def _pick(self, player: Player, destination: Block) -> None:
         target: Piece = destination.get_piece()
-        # todo destinationにある駒(target)を相手のpiecesから削除 
-        opponent: Player = [p for p in self.__players if p.get_name() != player.get_name()][0]
+        # todo destinationにある駒(target)を相手のpiecesから削除
+        opponent: Player = [
+            p for p in self.__players if p.get_name() != player.get_name()
+        ][0]
         for key, piece in opponent.pieces.items():
             if piece == target:
                 opponent.pieces.pop(key)
@@ -121,14 +153,20 @@ class Table:
 
     # 自分のコマを動かすメソッド
     def move(self, player: Player, player_piece: Piece, destination: Block) -> None:
-        # 仮置き オンライン対戦時に名前の衝突が起きた場合バグを生む可能性        
+        # 仮置き オンライン対戦時に名前の衝突が起きた場合バグを生む可能性
         assert player_piece.get_owner == player.get_name(), "Only YOUR piece can move."
         # 青いオバケの現在位置が脱出マスでdestinationも脱出マスであれば勝利
         if player_piece.get_type() == "blue":
-            if destination.is_escape_block() and player_piece.get_position() == destination.get_address():
+            if (
+                destination.is_escape_block()
+                and player_piece.get_position() == destination.get_address()
+            ):
                 self.__winner = player.get_name()
-                return 
-        if destination.get_piece is not None and destination.get_piece().get_owner() != player_piece.get_owner():
+                return
+        if (
+            destination.get_piece is not None
+            and destination.get_piece().get_owner() != player_piece.get_owner()
+        ):
             self._pick(player, destination)
         # 移動元のブロックからコマを削除
         curent_position: list[int] = player_piece.get_position()
@@ -136,8 +174,10 @@ class Table:
         # 移動先のブロックにコマを配置
         destination_position: list[int] = destination.get_address()
         player_piece.set_position(destination_position)
-        self.__table[destination_position[0]][destination_position[1]].set_piece(player_piece)
-    
+        self.__table[destination_position[0]][destination_position[1]].set_piece(
+            player_piece
+        )
+
     def _is_movable(self, piece: Piece, destination: Block) -> bool:
         # 現在位置の上下左右1マスより離れていたら移動不可
         current_position: list[int] = piece.get_position()
@@ -147,27 +187,31 @@ class Table:
         if x_diff > 1 and y_diff > 1:
             return False
         # 移動しないという選択肢はなし(青いオバケが脱出する場合は例外)
-        if x_diff == 0 and y_diff == 0 and not (destination.is_escape_block() and piece.get_type() == "blue"):
+        if (
+            x_diff == 0
+            and y_diff == 0
+            and not (destination.is_escape_block() and piece.get_type() == "blue")
+        ):
             return False
         # destinationに自分のコマがある場合は移動不可
         if piece.get_owner() == destination.get_piece().get_owner():
             return False
         return True
 
-    #ゲームの進行を行うメソッド
+    # ゲームの進行を行うメソッド
     def play(self) -> None:
         print("Game Start!")
         # 各プレイヤーがコマの初期位置を設定する
         # オフラインモードではCPUのコマは自動で設定する
         # オンラインモードでは各プレイヤーが同時に設定できるように非同期処理を使用する
         # 一方のプレイヤーが配置完了した後に、もう一方のプレイヤーが配置をするように制御するのはUXが悪い
-        while(self.__winner):
+        while self.__winner:
             for player in self.players:
-                # todo 
+                # todo
                 # not _is_movable の時はアラートと共に、
                 # _is_movableになるまで再度移動場所を指定させる
                 # フロントエンド実装段階ではマスをタップすることでそこにあるコマを引数pieceに渡す
-                # そして、pieceを選択した状態で行き先のマスをタップするとdestinationに渡すようにする                
+                # そして、pieceを選択した状態で行き先のマスをタップするとdestinationに渡すようにする
                 if self._is_movable:
                     # piece, destinationは仮置き
                     self.move(player, Piece(player.get_name(), "blue"), Block(3, 4))
--- env/boardGameProject/backend/geister/views.py
+++ env/boardGameProject/backend/geister/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
 from rest_framework import api_view
 from rest_framework.response import Response
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def move_piece(request) -> Response:
     pass
