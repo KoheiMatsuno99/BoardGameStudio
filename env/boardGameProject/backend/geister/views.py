654995af0e59139a1f2901f0a7ccacae860b150a
kohei <koheimtri3@gmail.com>
2023-05-07 22:31:36 +0900

    add api_view temp

--- env/boardGameProject/backend/geister/views.py
+++ env/boardGameProject/backend/geister/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def move_piece(request) -> Response:
+    pass
