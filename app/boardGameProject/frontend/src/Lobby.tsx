2a1e839dacad1f98e84c9b3cf22fc69c7a886ef8
kohei <koheimtri3@gmail.com>
2023-05-21 18:17:12 +0900

    add single mode btn

--- app/boardGameProject/frontend/src/Lobby.tsx
+++ app/boardGameProject/frontend/src/Lobby.tsx
@@ -4,7 +4,9 @@ import styles from "./Lobby.module.css";
 const Lobby: React.FC = () => {
     return (
         <div className={styles.container}>
-         <div className={styles.background}></div>  
+            <div className={styles.background}>
+                <button className={styles.button} id="single-mode-btn">Single Mode</button>
+            </div>  
         </div>
     )
 }
