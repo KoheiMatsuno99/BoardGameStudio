cb1448e15acfe356623e8821ef2a5510151fffba
kohei <koheimtri3@gmail.com>
2023-05-21 19:36:04 +0900

    GeisterRule <-> Lobby

--- app/boardGameProject/frontend/src/Lobby.tsx
+++ app/boardGameProject/frontend/src/Lobby.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import styles from "./Lobby.module.css";
+import GeisterRule from "./GeisterRule";
 
 const Lobby: React.FC = () => {
+    const [showGeisterRule, setGeisterRule] = React.useState(false);
+    const handleClick = () => {
+        setGeisterRule(true);
+    }
     return (
         <div className={styles.container}>
             <div className={styles.background}>
-                <button className={styles.button} id="single-mode-btn">Single Mode</button>
-            </div>  
+                {
+                    showGeisterRule ? <GeisterRule /> : (
+                        <button className={styles.button} id="single-mode-btn" onClick={handleClick}>Single Mode</button>
+                    )
+                }
+                
+            </div>
+        
+              
         </div>
     )
 }
