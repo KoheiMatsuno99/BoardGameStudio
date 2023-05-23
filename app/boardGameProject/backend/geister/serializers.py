b03b29ce8a479958762be06de13f1b37a3439a06
kohei <koheimtri3@gmail.com>
2023-05-23 23:11:46 +0900

    add BlockSerializer

--- app/boardGameProject/backend/geister/serializers.py
+++ app/boardGameProject/backend/geister/serializers.py
@@ -3,9 +3,23 @@ from rest_framework import serializers
 from .geister import Player, Table
 
 
+class BlockSerializer(serializers.Serializer):
+    position = serializers.ListField(
+        child=serializers.IntegerField(min_value=0, max_value=7)
+    )
+    piece = serializers.StringRelatedField(allow_null=True)
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
+
 class TableSerializer(serializers.Serializer):
     players = serializers.StringRelatedField(many=True)
     winner = serializers.CharField()
+    table = serializers.ListField(child=BlockSerializer(many=True))
 
     def create(self, validated_data) -> Table:
         return Table(**validated_data)
@@ -16,7 +30,6 @@ class TableSerializer(serializers.Serializer):
 
 class PlayerSerializer(serializers.Serializer):
     name = serializers.CharField()
-    pieces = serializers.StringRelatedField(many=True)
 
     def create(self, validated_data) -> Player:
         return Player(**validated_data)
