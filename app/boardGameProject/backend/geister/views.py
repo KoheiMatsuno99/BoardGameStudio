f8b00f0289ac4402fe4a8abc6140710e2cf34d07
kohei <koheimtri3@gmail.com>
2023-05-24 23:28:44 +0900

    mypyエラー解消

--- app/boardGameProject/backend/geister/views.py
+++ app/boardGameProject/backend/geister/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
 from typing import Optional
 
-from rest_framework import api_view, status
+from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.request import Request
 
