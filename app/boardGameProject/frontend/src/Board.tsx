055a39dd39af8a1aea7dae1a9008797b2ddcecea
kohei <koheimtri3@gmail.com>
2023-05-29 15:00:45 +0900

    refactor Board.tsx

--- app/boardGameProject/frontend/src/Board.tsx
+++ app/boardGameProject/frontend/src/Board.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Board.module.css";
 
 interface Piece {
@@ -12,55 +12,66 @@ interface Block {
     piece: Piece | null;
 }
 
+interface Player {
+    name: string;
+    pieces: Piece[];
+}
+
 interface BoardProps {
-    // todo: define initialData type
     initialData: any;
 }
 
 const initialBoard: Block[][] = Array.from({ length: 8 }, (_, i) => Array.from({ length: 8 }, (_, j) => ({ address: [i, j], piece: null })));
 
+const PieceDisplay: React.FC<{pieces: Piece[], player: Player, handlePieceClick: Function}> = ({pieces, player, handlePieceClick}) => (
+    <div className={styles.dFlex}>
+        {pieces.filter(piece => piece.owner === player.name).map((piece , index) => (
+            <img key={player.name + index} src={`../img/${piece.type}Ghost.jpeg`} className={styles.ghostImg} onClick={() => handlePieceClick(piece)}></img>
+        ))}
+    </div>
+);
+
+const BoardRow: React.FC<{row: Block[], handleBlockClick: Function}> = ({row, handleBlockClick}) => (
+    <div className={styles.row}>
+        {row.map((square, col_i) => (
+            <div key={'col' + col_i} className={styles.block} onClick={() => handleBlockClick(square)}>
+                {square.piece && <img src={`../img/${square.piece.type}Ghost.jpeg`} className={styles.ghostImgSmall} />}
+            </div>
+        ))}
+    </div>
+);
+
 const Board: React.FC<BoardProps> = ({initialData}) => {
-    const [selectedPiece, setSelectedPiece] = React.useState<Piece | null>(null);
-    const [board, setBoard] = React.useState<Block[][]>(initialBoard);
-    const [playerUnsetPieces, setPlayerPieces] = React.useState<Piece[][]>([initialData["players"][0]["pieces"], initialData["players"][1]["pieces"]]);
+    const [selectedPiece, setSelectedPiece] = useState<Piece | null>(null);
+    const [board, setBoard] = useState<Block[][]>(initialBoard);
+    const [playerUnsetPieces, setPlayerPieces] = useState<Piece[][]>([initialData.players[0].pieces, initialData.players[1].pieces]);
+
     const handlePieceClick = (piece: Piece) => {
         setSelectedPiece(piece);
         setPlayerPieces(playerUnsetPieces => playerUnsetPieces.map(pieces => pieces.filter(p => p !== piece)));
     }
+
     const handleBlockClick = (block: Block) => {
         if (!selectedPiece) return;
         if (block.piece) return;
         setBoard(board => board.map(row => row.map(b => b === block ? { ...b, piece: selectedPiece } : b)));
         setSelectedPiece(null);
     }
+
     return (
         <div className={styles.container}>
             <div className={styles.capturedPiecesTop}>
-                <div>{initialData["players"][1]["name"]}</div>          
-                <div className={styles.dFlex}>
-                    {playerUnsetPieces[0].filter((piece: Piece) => piece.owner === initialData["players"][0]["name"]).map((piece: Piece , index: number) => (
-                        <img key={initialData["players"][0]["name"] + index} src={`../img/${piece.type}Ghost.jpeg`} className={styles.ghostImg} onClick={() => handlePieceClick(piece)}></img>
-                    ))}
-                </div>
+                <div>{initialData.players[1].name}</div>          
+                <PieceDisplay pieces={playerUnsetPieces[0]} player={initialData.players[0]} handlePieceClick={handlePieceClick} />
             </div>
             <div className={styles.board}>
                 {board.map((row, row_i) => (
-                    <div key={'row' + row_i} className={styles.row}>
-                        {row.map((square, col_i) => (
-                            <div key={'col' + col_i} className={styles.block} onClick={() => handleBlockClick(square)}>
-                                {square.piece && <img src={`../img/${square.piece.type}Ghost.jpeg`} className={styles.ghostImgSmall} />}
-                            </div>
-                        ))}
-                    </div>
+                    <BoardRow key={'row' + row_i} row={row} handleBlockClick={handleBlockClick} />
                 ))}
             </div>
             <div className={styles.capturedPiecesBottom}>
-                <div className={styles.dFlex}>
-                    {playerUnsetPieces[1].filter((piece: Piece) => piece.owner === initialData["players"][1]["name"]).map((piece: Piece , index: number) => (
-                        <img key={initialData["players"][1]["name"] + index} src={`../img/${piece.type}Ghost.jpeg`} className={styles.ghostImg} onClick={() => handlePieceClick(piece)}></img>
-                    ))}
-                </div>
-                <div>{initialData["players"][0]["name"]}</div>
+                <PieceDisplay pieces={playerUnsetPieces[1]} player={initialData.players[1]} handlePieceClick={handlePieceClick} />
+                <div>{initialData.players[0].name}</div>
             </div>
         </div>
     )
