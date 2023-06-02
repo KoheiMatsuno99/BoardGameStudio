1ac8b5ec380c3b6fdc1591e63f5fa60838c3a830
kohei <koheimtri3@gmail.com>
2023-06-02 17:51:28 +0900

    notifyGetReady, picked_colors_pieces_count

--- app/boardGameProject/frontend/src/BoardController.ts
+++ app/boardGameProject/frontend/src/BoardController.ts
@@ -1,12 +1,28 @@
+import {Block} from './Board';
 import axios from 'axios';
+
 export class ApiGateway{
     public static async initializeGame(player1: string, player2: string){
         const playerData = [
-            {"name": player1},
-            {"name": player2}
+            {
+                "name": player1,
+                "picked_blue_pieces_count": 0,
+                "picked_red_pieces_count": 0
+            },
+            {
+                "name": player2,
+                "picked_blue_pieces_count": 0,
+                "picked_red_pieces_count": 0
+            }
         ];
         //todo リクエスト先をlocalhostから変更する
-        const response = await axios.post('http://localhost:8000/start_game/', playerData);
+        const response = await axios.post('http://localhost:8000/start/', playerData, { withCredentials: true })
+        return response.data;
+    }
+    public static async notifyGetReady(boardInfo: Block[][]){
+        //全てのコマの初期位置が確定したらコマの位置情報をサーバーに送信する          
+        const response = await axios.post('http://localhost:8000/setup/',boardInfo, {withCredentials: true});
+        console.log(response)
         return response.data;
     }
     public static async movePiece(player: string, player_piece: string, destination: string){
