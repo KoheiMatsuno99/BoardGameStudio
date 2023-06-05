5b230da60fe8953403bd1b1cc4e98380a2882ec4
kohei <koheimtri3@gmail.com>
2023-06-05 15:18:40 +0900

    初期配置にバリデーションを追加

--- app/boardGameProject/frontend/src/BoardState.ts
+++ app/boardGameProject/frontend/src/BoardState.ts
@@ -44,6 +44,25 @@ const useBoardState = (initialData: Table) => {
     const handleBlockClick = (block: Block) => {
         if (!selectedPiece) return;
         if (block.piece) return;
+        const playerIndex = players.findIndex(player => player.name === selectedPiece.owner);
+        const validPlacementRange = [
+            {"rowStart": 0, "rowEnd": 1, "colStart": 0, "colEnd": 7},
+            {"rowStart": 6, "rowEnd": 7, "colStart": 0, "colEnd": 7},
+        ]
+        const {rowStart, rowEnd, colStart, colEnd} = validPlacementRange[playerIndex]
+        const [selectedRow, selectedCol] = block.address;
+        if(selectedRow < rowStart || selectedRow > rowEnd || selectedCol < colStart || selectedCol > colEnd){
+            alert("初期位置は手前の2行の範囲にコマを置いてください");
+            return;
+        }
+        else if(playerIndex === 0 && selectedRow === rowStart && (selectedCol === colStart || selectedCol === colEnd)){
+            alert("脱出マスを初期位置に設定することはできません")
+            return;
+        }
+        else if(playerIndex === 1 && selectedRow === rowEnd && (selectedCol === colStart || selectedCol === colEnd)){
+            alert("脱出マスを初期位置に設定することはできません")
+            return;
+        }
         let pieceOfPositionUpdated = {...selectedPiece, position: block.address}
         setBoardInfo(board => board.map(row => row.map(b => b === block ? { ...b, piece:  pieceOfPositionUpdated} : b)));
         setPlayerPieces(playerUnsetPieces => playerUnsetPieces.map(pieces => pieces.filter(p => p !== selectedPiece)));
