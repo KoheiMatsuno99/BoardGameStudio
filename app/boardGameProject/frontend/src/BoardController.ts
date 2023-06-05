7d6d44e33cf14655cb996c2e10049149c3694ed8
kohei <koheimtri3@gmail.com>
2023-06-05 19:47:34 +0900

    implement movePiece request

--- app/boardGameProject/frontend/src/BoardController.ts
+++ app/boardGameProject/frontend/src/BoardController.ts
@@ -1,8 +1,8 @@
-import {Table} from './BoardState';
+import {Table, Player, Piece, Block} from './BoardState';
 import axios from 'axios';
 
 export class ApiGateway{
-    public static async initializeGame(player1: string, player2: string){
+    public static async initializeGame(player1: string, player2: string): Promise<Table>{
         const playerData = [
             {
                 "name": player1,
@@ -19,13 +19,19 @@ export class ApiGateway{
         const response = await axios.post('http://localhost:8000/start/', playerData, { withCredentials: true })
         return response.data;
     }
-    public static async notifyGetReady(tableInfo: Table){
+    public static async notifyGetReady(tableInfo: Table): Promise<Table>{
         //全てのコマの初期位置が確定したらコマの位置情報をサーバーに送信する          
         const response = await axios.post('http://localhost:8000/setup/',tableInfo, {withCredentials: true});
         console.log(response)
         return response.data;
     }
-    public static async movePiece(player: string, player_piece: string, destination: string){
-        
+    public static async movePiece(players: Player[], player_piece: Piece, destination: Block): Promise<Table>{
+        const movementInfo = {
+            "players": players,
+            "player_piece": player_piece,
+            "destination": destination
+        }
+        const response = await axios.post('http://localhost:8000/movement/', movementInfo, {withCredentials: true});
+        return response.data;
     }
 }
