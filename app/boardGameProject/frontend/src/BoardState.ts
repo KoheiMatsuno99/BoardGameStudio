1477a98721e8a32e3eaad0b927155383db833c6a
kohei <koheimtri3@gmail.com>
2023-06-20 18:06:26 +0900

    Separate validation from handleMovement

--- app/boardGameProject/frontend/src/BoardState.ts
+++ app/boardGameProject/frontend/src/BoardState.ts
@@ -94,6 +94,24 @@ const useBoardState = (initialData: Table) => {
         setSelectedPiece(null);
     }
 
+    const isAdjacentBlock = (piecePosition: number[], blockPosition: number[]) => 
+    Math.abs(blockPosition[0] - piecePosition[0]) + Math.abs(blockPosition[1] - piecePosition[1]) === 1;
+
+    const isSameBlock = (piecePosition: number[], blockPosition: number[]) =>
+        blockPosition[0] === piecePosition[0] && blockPosition[1] === piecePosition[1];
+
+    const validateMovement = (selectedPiece: Piece, block: Block) => {
+        if(!isAdjacentBlock(selectedPiece.position, block.address)){
+            alert("隣接するマスにしか移動できません");
+            return false;
+        }
+        if(isSameBlock(selectedPiece.position, block.address)){
+            alert("同じマスには移動できません");
+            return false;
+        }
+        return true;
+    }
+
     const handleMovement = (block: Block) => {
         if(selectedPiece === null){
             alert("コマを選択してください");
@@ -103,17 +121,14 @@ const useBoardState = (initialData: Table) => {
             alert("そのマスにはコマがすでに存在します");
             return;
         }
+        /*
+        デバッグ用メッセージ 
         console.log("Selected piece: ", selectedPiece);
         console.log("Players: ", players);
+        */
+       
         //移動におけるバリデーション
-        const isAdjacent = Math.abs(block.address[0] - selectedPiece.position[0]) + Math.abs(block.address[1] - selectedPiece.position[1]) === 1;
-        const isSamePosition = block.address[0] === selectedPiece.position[0] && block.address[1] === selectedPiece.position[1];
-        if(!isAdjacent){
-            alert("隣接するマスにしか移動できません");
-            return;
-        }
-        if(isSamePosition){
-            alert("同じマスには移動できません");
+        if(!validateMovement(selectedPiece, block)){
             return;
         }
         /* todo turnによる制御 
