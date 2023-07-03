20bfb9f6b7d4f34cbd9909cabc65dd6f2d7d7a94
kohei <koheimtri3@gmail.com>
2023-07-03 16:43:06 +0900

    sys.pathに/home/ubuntu/boardGameProjectを追加

--- app/boardGameProject/backend/boardGameProject/wsgi.py
+++ app/boardGameProject/backend/boardGameProject/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
-
+path = '/home/ubuntu/boardGameProject'
+if path not in sys.path:
+    sys.path.append('/home/ubuntu/boardGameProject')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boardGameProject.settings")
 
 application = get_wsgi_application()
