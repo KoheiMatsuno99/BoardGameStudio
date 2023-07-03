033618753e8c8c99cd0579026ead56680a114df2
kohei <koheimtri3@gmail.com>
2023-07-03 16:55:02 +0900

    sys.path.append('app/boardGameProject/backend/boardGameProject')

--- app/boardGameProject/backend/boardGameProject/wsgi.py
+++ app/boardGameProject/backend/boardGameProject/wsgi.py
@@ -11,9 +11,8 @@ import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
-path = '/home/ubuntu/boardGameProject'
-if path not in sys.path:
-    sys.path.append('/home/ubuntu/boardGameProject')
+
+sys.path.append('app/boardGameProject/backend/boardGameProject')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boardGameProject.settings")
 
 application = get_wsgi_application()
