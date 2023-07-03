c091411973f12fbcc939a268f55d491bfa1c9d22
kohei <koheimtri3@gmail.com>
2023-07-03 22:02:36 +0900

    remigrate

--- app/boardGameProject/backend/boardGameProject/settings.py
+++ app/boardGameProject/backend/boardGameProject/settings.py
@@ -7,7 +7,7 @@ env_path = f".env.{os.getenv('DJANGO_ENV', 'prod')}"
 load_dotenv(env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
