ffd2a7bb532901e63f204469c7a5f3138fc7cb88
kohei <koheimtri3@gmail.com>
2023-07-04 23:03:36 +0900

    request urlを変更

--- app/boardGameProject/frontend/src/BoardController.ts
+++ app/boardGameProject/frontend/src/BoardController.ts
@@ -16,7 +16,7 @@ export class ApiGateway {
     ];
     //todo リクエスト先をlocalhostから変更する
     const response = await axios.post(
-      "https://BoardGameStudio.ap-northeast-1.elasticbeanstalk.com/start/",
+      "https://board-game-studio.net/start/",
       playerData,
       { withCredentials: true }
     );
@@ -32,7 +32,7 @@ export class ApiGateway {
   public static async notifyGetReady(tableInfo: Table): Promise<Table> {
     //全てのコマの初期位置が確定したらコマの位置情報をサーバーに送信する
     const response = await axios.post(
-      "https://BoardGameStudio.ap-northeast-1.elasticbeanstalk.com/setup/",
+      "https://board-game-studio.net/setup/",
       tableInfo,
       { withCredentials: true }
     );
@@ -57,7 +57,7 @@ export class ApiGateway {
       destination: destination,
     };
     const response = await axios.post(
-      "https://BoardGameStudio.ap-northeast-1.elasticbeanstalk.com/movement/",
+      "https://board-game-studio.net/movement/",
       movementInfo,
       { withCredentials: true }
     );
@@ -68,7 +68,7 @@ export class ApiGateway {
   }
   public static async cpuMovePiece(): Promise<Table> {
     const response = await axios.post(
-      "https://BoardGameStudio.ap-northeast-1.elasticbeanstalk.com/cpu-movement/",
+      "https://board-game-studio.net/cpu-movement/",
       {},
       { withCredentials: true }
     );
